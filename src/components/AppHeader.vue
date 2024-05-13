f8e9427f317ee064a6c4fc7527733396859b4e1c
MattiaMessana <mattia.messana@icloud.com>
2024-05-13 12:51:17 +0200

    add:appHeader navigation

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,29 @@
 <script>
 export default {
+    data() {
+        return {
+            menu: [
+                {
+                    title: "Home",
+                    routeName: "home",
+                }
+            ]
+        }
+    }
 
 }
 </script>
 
 <template>
-    Header
+    <header>
+        <ul>
+            <li v-for="item in menu"> 
+                <router-link :to="{name: item.routeName}" >
+                    {{ item.title }}
+                </router-link>
+             </li>
+        </ul>
+    </header>
 </template>
 
 <style scoped lang="scss">
