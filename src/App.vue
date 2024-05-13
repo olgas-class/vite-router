23449e2e36e3fe71f41de2b8ef313bcec7bb3301
GianlivioIemolo <gianlivioiemolo@gmail.com>
2024-05-13 12:19:33 +0200

    feat: Header component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,16 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+
 export default {
+   components: {
+        AppHeader,
+   }
 
 }
 </script>
 
 <template>
+  <AppHeader />
   <h1>Ciao</h1>
 </template>
 
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+
+}
+</script>
+
+<template>
+    Header
+</template>
+
+<style scoped lang="scss">
+</style>
